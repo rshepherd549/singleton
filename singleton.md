1b09582ac27aeb76568695e925114314fac34b2c
Richard Shepherd <rshepherd@esri.com>
2023-06-26 18:35:23 +0100

    slides with code

--- singleton.md
+++ singleton.md
@@ -1,4 +1,96 @@
-# Singleton
+# When Singletons go bad..
 
+## Richard Shepherd
 
+---
 
+# Simple thread safe construction for a shared resource
+`static` object is only constructed once
+
+```cpp
+class Manager1
+{
+public:
+  static Manager1& Instance() {
+    static Manager1 manager;
+    return manager;
+  }
+  std::string const * GetResource() const {
+```
+
+---
+
+# Uncertain initialization?
+
+```cpp
+class Manager3
+{
+  Resource * resource_;
+  Manager3(): resource_{CreateResource()}{}
+  ~Manager3() {
+    if (resource_)
+      DestroyResource(resource_);
+  }
+public:
+  static Manager3& Instance() {
+    static Manager3 manager;
+    return manager;
+  }
+  Resource const * GetResource() const {
+```
+
+---
+
+# Only one chance for construction
+
+```cpp
+  if (const auto res = Manager3::Instance().GetResource())
+    DoWork(res);
+  if (const auto res = Manager3::Instance().GetResource())
+    DoWork(res);
+```
+
+---
+
+# Fixes
+
+- for each use: check `IsValid` and `ReInit`
+- repeated creation with manual flag and mutex
+- extensions: `run_once`
+
+---
+
+# Re-examine static construction
+`static` object only **completes** construction once.
+
+Throwing an exception interrupts construction.
+
+```cpp
+class Manager4
+{
+  Manager4() {
+    resource_ = CreateResource();
+    if (!resource_)
+      throw std::exception{"Not ready"};
+  }
+public:
+  static Manager4* Instance() {
+    try {
+      static Manager4 manager;
+      return &manager;
+    }
+    catch(...) {
+      return nullptr;
+    }
+```
+
+---
+
+# API forces checking singleton's readiness
+
+```cpp
+  if (const auto mgr = Manager4::Instance())
+    DoWork(mgr->GetResource());
+  if (const auto mgr = Manager4::Instance())
+    DoWork(mgr->GetResource());
+```
--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 
 template< typename T>
-void DoWork(const T& handle)
-{
+void DoWork(const T& handle) {
   std::cout << *handle << "\n";
 }
 
 class Manager1
 {
-  Manager1()
-  {
+  Manager1(){
   }
 public:
-  static Manager1& Instance()
-  {
+  static Manager1& Instance() {
     static Manager1 manager;
     return manager;
   }
-  std::string const * GetResource() const
-  {
+  std::string const * GetResource() const {
     static std::string resource{"Hello!"};
     return &resource;
   }
@@ -30,16 +26,12 @@ class Manager2
 {
   std::unique_ptr<int> resource_;
   Manager2(): resource_{new int{42}}
-  {
-  }
 public:
-  static Manager2& Instance()
-  {
+  static Manager2& Instance() {
     static Manager2 manager;
     return manager;
   }
-  int const * GetResource() const
-  {
+  int const * GetResource() const {
     return resource_.get();
   }
 };
@@ -47,38 +39,32 @@ public:
 //////////////////////
 
 struct Resource{int value_{43};};
-std::ostream& operator<<(std::ostream& os, const Resource& res){ return os << res.value_ << "\n";}
+std::ostream& operator<<(std::ostream& os, const Resource& res){ return os << res.value_;}
 
-Resource * CreateResource()
-{
+Resource * CreateResource() {
   static int count = 0;
-  return count++ ? new Resource{} : nullptr;
+  return count++ < 1 ? nullptr : new Resource{};
 }
-void DestroyResource(Resource* res)
-{
+void DestroyResource(Resource* res) {
   delete res;
 }
 
 class Manager3
 {
   Resource * resource_;
-  Manager3()
-  {
+  Manager3() {
     resource_ = CreateResource();
   }
-  ~Manager3()
-  {
+  ~Manager3() {
     if (resource_)
       DestroyResource(resource_);
   }
 public:
-  static Manager3& Instance()
-  {
+  static Manager3& Instance() {
     static Manager3 manager;
     return manager;
   }
-  Resource const * GetResource() const
-  {
+  Resource const * GetResource() const {
     return resource_;
   }
 };
@@ -88,45 +74,50 @@ public:
 class Manager4
 {
   Resource * resource_;
-  Manager4()
-  {
+  Manager4() {
     resource_ = CreateResource();
     if (!resource_)
       throw std::exception{"Not ready"};
   }
-  ~Manager4()
-  {
+  ~Manager4() {
     if (resource_)
       DestroyResource(resource_);
   }
 public:
-  static Manager4* Instance()
-  {
-    try
-    {
+  static Manager4* Instance() {
+    try {
       static Manager4 manager;
       return &manager;
     }
-    catch(...)
-    {
+    catch(...) {
       return nullptr;
     }
   }
-  Resource const * GetResource() const
-  {
+  Resource const * GetResource() const {
     return resource_;
   }
 };
 
 //////////////////////
 
-int main()
-{
+int main() {
   std::cout << "Start\n";
+
+  //DoWork(Manager1::Instance().GetResource());
+
+  //DoWork(Manager2::Instance().GetResource());
+
   //DoWork(Manager3::Instance().GetResource());
+
+  //if (const auto res = Manager3::Instance().GetResource())
+  //  DoWork(res);
+  //if (const auto res = Manager3::Instance().GetResource())
+  //  DoWork(res);
+
   if (const auto mgr = Manager4::Instance())
     DoWork(mgr->GetResource());
   if (const auto mgr = Manager4::Instance())
     DoWork(mgr->GetResource());
+
   std::cout << "Finish\n";
 }
